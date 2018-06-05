6ec1660342603699e6901050f431a957c8603e06
Richard Melchior <raustin.melchior@gmail.com>
2018-06-05 17:53:45 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,14 +1,15 @@
 class CashRegister
 
-  attr_accessor :total, :discount
+  attr_accessor :total, :discount, :items
 
   def initialize(discount = 0)
     @total = 0;
     @discount = discount;
   end
 
-  def self.cash_register_with_discount(discount)
-    @discount = discount;
+  def add_item(item, price, quantity = 1)
+    @items << item;
+    @total += price * quantity;
   end
 
 end
