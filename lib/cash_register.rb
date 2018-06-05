d75cbd0c77bc27a623220dbdaee13fd38ccf9109
Richard Melchior <raustin.melchior@gmail.com>
2018-06-05 18:19:51 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -4,7 +4,7 @@ class CashRegister
 
   #@items = []
 
-  def initialize(discount = 0)
+  def initialize(discount = 0.0)
     @total = 0.0;
     @discount = discount;
     @items = []
@@ -13,6 +13,13 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     quantity > 1 ? quantity.times{@items << item} : @items << item; 
     @total += price * quantity;
+  end
+
+  def apply_discount
+    @total *= @discount;
+    @discount == 0.0 ? "There is not discount to apply." :
+                       "Your new discount is #{@total}";
+    
   end
 
 end
