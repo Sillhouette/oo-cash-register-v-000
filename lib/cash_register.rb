4322fbab9f4a2f8ce37cadb2934c7f34e6447fd1
Richard Melchior <raustin.melchior@gmail.com>
2018-06-05 17:04:17 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,8 +1,13 @@
 class CashRegister
 
-  
+  attr_accessor :total, :discount
 
   def initialize
-
+    @total = 0;
   end
+
+  def self.cash_register_with_discount(discount)
+    @discount = discount;
+  end
+
 end
